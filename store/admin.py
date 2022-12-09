744be5b2af723a824282d7213f3b9ba806378501
javohir6666 <javohir6666@gmail.com>
2022-12-09 15:47:00 +0500

    category model added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category
 # Register your models here.
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'slug')
+    list_filter = ('status', 'trending')
+    search_fields = ['title', 'meta_keywords']
+admin.site.register(Category, AdminCategory)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from autoslug import AutoSlugField
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(max_length=500,null=False, blank=False)
+    slug = AutoSlugField(populate_from='title')
+    image = models.ImageField(upload_to='uploads/category_photos/', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False, help_text="0=default ,1=Hidden")
+    trending = models.BooleanField(default=False, help_text="0=default ,1=Trending")
+    meta_title = models.CharField(max_length=150, null=False, blank=False)
+    meta_keywords = models.CharField(max_length=150, null=False, blank=False)
+    meta_description = models.TextField(max_length=500,null=False, blank=False)
+    
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+        
+    def __str__(self):
+        return self.title
+    
+    
