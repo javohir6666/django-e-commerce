4f1e397347520b1750346c041abdc11a336d76ba
javohir6666 <javohir6666@gmail.com>
2022-12-09 16:00:29 +0500

    product model added

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 # Register your models here.
 class AdminCategory(admin.ModelAdmin):
-    list_display = ('title', 'description', 'created_at', 'slug')
+    list_display = ('title', 'created_at', 'slug')
     list_filter = ('status', 'trending')
     search_fields = ['title', 'meta_keywords']
 admin.site.register(Category, AdminCategory)
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'slug', 'selling_price')
+    list_filter = ('status', 'trending', 'selling_price')
+    search_fields = ['title', 'meta_keywords', 'selling_price']
+admin.site.register(Product, AdminProduct)
--- store/models.py
+++ store/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from autoslug import AutoSlugField
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(max_length=500,null=False, blank=False)
     slug = AutoSlugField(populate_from='title')
-    image = models.ImageField(upload_to='uploads/category_photos/', null=False, blank=False)
+    image = models.ImageField(upload_to='uploads/category_photos/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False, help_text="0=default ,1=Hidden")
@@ -22,4 +23,32 @@ class Category(models.Model):
     def __str__(self):
         return self.title
     
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(max_length=500,null=False, blank=False)
+    slug = AutoSlugField(populate_from='title')
+    product_image = models.ImageField(upload_to='uploads/product_photos/', null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False)
+    original_price = models.FloatField(null=False, blank=False)
+    selling_price = models.FloatField(null=False, blank=False)
+    tag = models.CharField(max_length=150, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False, help_text="0=default ,1=Hidden")
+    trending = models.BooleanField(default=False, help_text="0=default ,1=Trending")
+    meta_title = models.CharField(max_length=150, null=False, blank=False)
+    meta_keywords = models.CharField(max_length=150, null=False, blank=False)
+    meta_description = models.TextField(max_length=500,null=False, blank=False)
     
+    
+    class Meta:
+        verbose_name_plural = 'Products'
+        
+        
+    def __str__(self):
+        return self.title
+    
+    # def get_absolute_url(self):
+    #     return reverse('store:ProductDetail', kwargs={'slug': self.slug})
